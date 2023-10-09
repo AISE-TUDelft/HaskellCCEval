c3ea35494e31dcaa895f44f5c9984d3f1a41ddfb
Frank van der Heijden <frank.boekanier@gmail.com>
2023-10-09 15:42:37 +0200

    Add Haskell implementation for HumanEval 91

--- humaneval-hs/HumanEval-91.hs
+++ humaneval-hs/HumanEval-91.hs
@@ -23,7 +23,23 @@
 
 
 -- Haskell Implementation:
+import Data.List
 
--- ???
-is_bored :: ???
-is_bored = ???
+-- You'll be given a string of words, and your task is to count the number
+-- of boredoms. A boredom is a sentence that starts with the word "I".
+-- Sentences are delimited by '.', '?' or '!'.
+
+-- For example:
+-- >>> is_bored "Hello world"
+-- 0
+-- >>> is_bored "The sky is blue. The sun is shining. I love this weather"
+-- 1
+is_bored :: String -> Int
+is_bored s = sum [1 | sentence <- splitOnDelimiters s, take 2 sentence == "I "]
+
+-- Helper function to split the string based on delimiters '.', '!', and '?'
+splitOnDelimiters :: String -> [String]
+splitOnDelimiters [] = []
+splitOnDelimiters s = case break (`elem` ".?!") s of
+    (sentence, [])       -> [sentence]
+    (sentence, (_:rest)) -> sentence : splitOnDelimiters (dropWhile (`elem` " ") rest)
