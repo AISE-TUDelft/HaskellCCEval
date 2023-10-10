34de0d715caa87aca9bb9658aff66d8bd6332b09
Philippe de Bekker <research@philippedebekker.com>
2023-10-10 14:14:19 +0200

    Enhanced old implementations using HLint

--- humaneval-hs/HumanEval-132.hs
+++ humaneval-hs/HumanEval-132.hs
@@ -3,13 +3,13 @@
 
 -- Python Implementation:
 
--- 
+--
 -- def is_nested(string):
 --     '''
 --     Create a function that takes a string as input which contains only square brackets.
---     The function should return True if and only if there is a valid subsequence of brackets 
+--     The function should return True if and only if there is a valid subsequence of brackets
 --     where at least one bracket in the subsequence is nested.
--- 
+--
 --     is_nested('[[]]') ➞ True
 --     is_nested('[]]]]]]][[[[[]') ➞ False
 --     is_nested('[][]') ➞ False
@@ -33,42 +33,41 @@
 --             cnt += 1
 --             i += 1
 --     return cnt >= 2
--- 
---     
--- 
-
+--
+--
+--
 
 -- Haskell Implementation:
-
-    -- Manually translated from Python implementation above.
-
-    -- Tested the following cases:
-    -- >>> is_nested "[[]]"
-    -- True
-    -- >>> is_nested "[]]]]]]][[[[[]"
-    -- False
-    -- >>> is_nested "[][]"
-    -- False
-    -- >>> is_nested "[]"
-    -- False
-    -- >>> is_nested "[[][]]"
-    -- True
-    -- >>> is_nested "[[]][["
-    -- True
+-- Create a function that takes a string as input which contains only square brackets.
+-- The function should return True if and only if there is a valid subsequence of brackets
+-- where at least one bracket in the subsequence is nested.
+--
+-- >>> is_nested "[[]]"
+-- True
+-- >>> is_nested "[]]]]]]][[[[[]"
+-- False
+-- >>> is_nested "[][]"
+-- False
+-- >>> is_nested "[]"
+-- False
+-- >>> is_nested "[[][]]"
+-- True
+-- >>> is_nested "[[]][["
+-- True
 
 is_nested :: String -> Bool
 is_nested string = is_nested' string 0 [] []
-    where
-        is_nested' :: String -> Int -> [Int] -> [Int] -> Bool
-        is_nested' ('[':cs) index opening_bracket_index closing_bracket_index = is_nested' cs (index + 1) (opening_bracket_index ++ [index]) closing_bracket_index
-        is_nested' (']':cs) index opening_bracket_index closing_bracket_index = is_nested' cs (index + 1) opening_bracket_index ([index] ++ closing_bracket_index)
-        is_nested' "" index opening_bracket_index closing_bracket_index
-            | index < 3 = False
-            | otherwise = is_nested'' opening_bracket_index closing_bracket_index 0 (length closing_bracket_index)
-                where
-                    is_nested'' :: [Int] -> [Int] -> Int -> Int -> Bool
-                    is_nested'' _ [] cnt l = cnt >= 2
-                    is_nested'' [] _ cnt l = cnt >= 2
-                    is_nested'' (o:os) (c:cs) cnt l
-                        | o < c && cnt < l = is_nested'' os cs (cnt + 1) l
-                        | otherwise = is_nested'' os (c:cs) cnt l
+  where
+    is_nested' :: String -> Int -> [Int] -> [Int] -> Bool
+    is_nested' ('[' : cs) index opening_bracket_index closing_bracket_index = is_nested' cs (index + 1) (opening_bracket_index ++ [index]) closing_bracket_index
+    is_nested' (']' : cs) index opening_bracket_index closing_bracket_index = is_nested' cs (index + 1) opening_bracket_index ([index] ++ closing_bracket_index)
+    is_nested' "" index opening_bracket_index closing_bracket_index
+      | index < 3 = False
+      | otherwise = is_nested'' opening_bracket_index closing_bracket_index 0 (length closing_bracket_index)
+      where
+        is_nested'' :: [Int] -> [Int] -> Int -> Int -> Bool
+        is_nested'' _ [] cnt l = cnt >= 2
+        is_nested'' [] _ cnt l = cnt >= 2
+        is_nested'' (o : os) (c : cs) cnt l
+          | o < c && cnt < l = is_nested'' os cs (cnt + 1) l
+          | otherwise = is_nested'' os (c : cs) cnt l
