d771f686aaafee6014441d15a01dc0d2df4a0a40
Philippe de Bekker <research@philippedebekker.com>
2023-10-12 11:09:01 +0200

    Implemented HumanEval #144

--- humaneval-hs/HumanEval-144.hs
+++ humaneval-hs/HumanEval-144.hs
@@ -3,15 +3,15 @@
 
 -- Python Implementation:
 
--- 
+--
 -- def simplify(x, n):
 --     """Your task is to implement a function that will simplify the expression
 --     x * n. The function returns True if x * n evaluates to a whole number and False
 --     otherwise. Both x and n, are string representation of a fraction, and have the following format,
 --     <numerator>/<denominator> where both numerator and denominator are positive whole numbers.
--- 
+--
 --     You can assume that x, and n are valid fractions, and do not have zero as denominator.
--- 
+--
 --     simplify("1/5", "5/1") = True
 --     simplify("1/6", "2/1") = False
 --     simplify("7/10", "10/2") = False
@@ -23,11 +23,33 @@
 --     if (numerator/denom == int(numerator/denom)):
 --         return True
 --     return False
--- 
-
+--
 
 -- Haskell Implementation:
 
--- ???
-simplify :: ???
-simplify = ???
+-- Your task is to implement a function that will simplify the expression
+-- x * n. The function returns True if x * n evaluates to a whole number and False
+-- otherwise. Both x and n, are string representation of a fraction, and have the following format,
+-- <numerator>/<denominator> where both numerator and denominator are positive whole numbers.
+--
+-- You can assume that x, and n are valid fractions, and do not have zero as denominator.
+--
+-- >>> simplify "1/5" "5/1"
+-- True
+-- >>> simplify "1/6" "2/1"
+-- False
+-- >>> simplify "7/10" "10/2"
+-- False
+
+import Data.List
+import Data.Maybe
+
+simplify :: String -> String -> Bool
+simplify x n = numerator / denominator == fromIntegral (round (numerator / denominator))
+  where
+    a = read (take (fromJust (elemIndex '/' x)) x) :: Double
+    b = read (drop (fromJust (elemIndex '/' x) + 1) x) :: Double
+    c = read (take (fromJust (elemIndex '/' n)) n) :: Double
+    d = read (drop (fromJust (elemIndex '/' n) + 1) n) :: Double
+    numerator = a * c
+    denominator = b * d
