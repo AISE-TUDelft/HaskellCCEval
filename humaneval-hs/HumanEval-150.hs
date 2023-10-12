42eea10c80ec888b01a4bbe768dd2118b15542b6
Philippe de Bekker <research@philippedebekker.com>
2023-10-12 12:07:58 +0200

    Implemented HumanEval #150

--- humaneval-hs/HumanEval-150.hs
+++ humaneval-hs/HumanEval-150.hs
@@ -3,15 +3,15 @@
 
 -- Python Implementation:
 
--- 
+--
 -- def x_or_y(n, x, y):
---     """A simple program which should return the value of x if n is 
+--     """A simple program which should return the value of x if n is
 --     a prime number and should return the value of y otherwise.
--- 
+--
 --     Examples:
 --     for x_or_y(7, 34, 12) == 34
 --     for x_or_y(15, 8, 5) == 5
---     
+--
 --     """
 --     if n == 1:
 --         return y
@@ -21,11 +21,17 @@
 --             break
 --     else:
 --         return x
--- 
-
+--
 
 -- Haskell Implementation:
 
--- ???
-x_or_y :: ???
-x_or_y = ???
+-- A simple program which should return the value of x if n is
+-- a prime number and should return the value of y otherwise.
+--
+-- Examples:
+-- >>> x_or_y 7 34 12
+-- 34
+-- >>> x_or_y 15 8 5
+-- 5
+x_or_y :: Int -> Int -> Int -> Int
+x_or_y n x y = if n == 1 then y else if length [i | i <- [2 .. n - 1], n `mod` i == 0] > 0 then y else x
