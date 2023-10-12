fb06888c5ada299249675c48c4c4dee059548b00
Philippe de Bekker <research@philippedebekker.com>
2023-10-12 12:04:26 +0200

    Implemented HumanEval #148

--- humaneval-hs/HumanEval-148.hs
+++ humaneval-hs/HumanEval-148.hs
@@ -3,18 +3,18 @@
 
 -- Python Implementation:
 
--- 
+--
 -- def bf(planet1, planet2):
 --     '''
---     There are eight planets in our solar system: the closerst to the Sun 
---     is Mercury, the next one is Venus, then Earth, Mars, Jupiter, Saturn, 
+--     There are eight planets in our solar system: the closest to the Sun
+--     is Mercury, the next one is Venus, then Earth, Mars, Jupiter, Saturn,
 --     Uranus, Neptune.
---     Write a function that takes two planet names as strings planet1 and planet2. 
---     The function should return a tuple containing all planets whose orbits are 
---     located between the orbit of planet1 and the orbit of planet2, sorted by 
---     the proximity to the sun. 
+--     Write a function that takes two planet names as strings planet1 and planet2.
+--     The function should return a tuple containing all planets whose orbits are
+--     located between the orbit of planet1 and the orbit of planet2, sorted by
+--     the proximity to the sun.
 --     The function should return an empty tuple if planet1 or planet2
---     are not correct planet names. 
+--     are not correct planet names.
 --     Examples
 --     bf("Jupiter", "Neptune") ==> ("Saturn", "Uranus")
 --     bf("Earth", "Mercury") ==> ("Venus")
@@ -29,11 +29,35 @@
 --         return (planet_names[planet1_index + 1: planet2_index])
 --     else:
 --         return (planet_names[planet2_index + 1 : planet1_index])
--- 
-
+--
 
 -- Haskell Implementation:
 
--- ???
-bf :: ???
-bf = ???
+-- There are eight planets in our solar system: the closest to the Sun
+-- is Mercury, the next one is Venus, then Earth, Mars, Jupiter, Saturn,
+-- Uranus, Neptune.
+-- Write a function that takes two planet names as strings planet1 and planet2.
+-- The function should return a tuple containing all planets whose orbits are
+-- located between the orbit of planet1 and the orbit of planet2, sorted by
+-- the proximity to the sun.
+-- The function should return an empty tuple if planet1 or planet2
+-- are not correct planet names.
+-- Examples
+-- >>> bf "Jupiter" "Neptune"
+-- ["Saturn", "Uranus"]
+-- >>> bf "Earth" "Mercury"
+-- ["Venus"]
+-- >>> bf "Mercury" "Uranus"
+-- ["Venus", "Earth", "Mars", "Jupiter", "Saturn"]
+bf :: String -> String -> [String]
+bf planet1 planet2 =
+  if planet1 `elem` planet_names && planet2 `elem` planet_names && planet1 /= planet2
+    then
+      if planet1_index < planet2_index
+        then take (planet2_index - planet1_index - 1) (drop (planet1_index + 1) planet_names)
+        else take (planet1_index - planet2_index - 1) (drop (planet2_index + 1) planet_names)
+    else []
+  where
+    planet_names = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+    planet1_index = head [i | (i, x) <- zip [0 ..] planet_names, x == planet1]
+    planet2_index = head [i | (i, x) <- zip [0 ..] planet_names, x == planet2]
