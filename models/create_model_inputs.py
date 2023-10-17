4205378d230bb1659fed5b615161bf68032c10c2
Tim van Dam <timvandamcs@gmail.com>
2023-10-17 16:18:03 +0200

    fix 2

--- models/create_model_inputs.py
+++ models/create_model_inputs.py
@@ -77,7 +77,7 @@ def create_unixcoder_dev(test):
                 continue
             full_code = preprocess_unixcoder(sample['full_code'])
             space_split = full_code.split(' ')
-            split_indices = [i for i in range(1, len(space_split) - 1) if not space_split[i:].strip().startswith('<EOL>')]
+            split_indices = [i for i in range(1, len(space_split) - 1) if not " ".join(space_split[i:]).strip().startswith('<EOL>')]
             split_index = random.choice(split_indices)
             model_input = ' '.join(space_split[:split_index])
             model_output = ' '.join(space_split[split_index:]).split('<EOL>')[0]
