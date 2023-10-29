c287c856789f683aedc60ed29469980ab061fd1b
Philippe de Bekker <research@philippedebekker.com>
2023-10-29 14:36:32 +0100

    Add percentages and filtered focus for taxonomy

--- models/evaluation/taxonomify.py
+++ models/evaluation/taxonomify.py
@@ -25,6 +25,9 @@ categories = {
 # The offset needed to match the indices of the first annotation in the Excel file
 sheet_offset = 2  # One for the header, one for the fact that Excel starts at 1
 
+# The maximum number of rows to process (- 1 since the first Excel row for reference is the header)
+row_limit = 302 - 1
+
 
 def main():
     parser = ArgumentParser()
@@ -40,15 +43,32 @@ def main():
             raise ValueError(f"File {file} does not exist.")
 
     for file in args.file:
-        df = get_excel_as_dataframe(file)
+        df = get_excel_as_dataframe(file).head(row_limit)
+
         taxonomy = get_taxonomy(df)
 
         # TODO: Do something with the taxonomy for the analysis of common pitfalls
         # Currently, playground:
 
-        # print(json.dumps(_get_em_or_valid_taxonomy_counts(taxonomy), indent=4))
+        # print(json.dumps(_get_no_em_and_invalid_taxonomy_counts(taxonomy), indent=4))
+        # print(json.dumps(get_taxonomy_percentages(
+        #     _get_no_em_and_invalid_taxonomy_counts(taxonomy)), indent=4))
+
         # print(json.dumps(get_cumulative_scores(
-        #     _get_em_or_valid_taxonomy_counts(taxonomy)), indent=4))
+        #     _get_no_em_and_invalid_taxonomy_counts(taxonomy)), indent=4))
+        # print(json.dumps(get_cumulative_percentages(
+        #     get_cumulative_scores(_get_no_em_and_invalid_taxonomy_counts(taxonomy))), indent=4))
+
+        # print("Unique rows:", _get_unique_rows_taxonomy_counts(
+        #     get_taxonomy(df), debug=True))
+
+        # print("Unique rows:", _get_unique_rows_taxonomy_counts(
+        #     get_taxonomy(df, include=lambda x: x[0] or x[2]), debug=False))
+
+        print(json.dumps(get_filtered_taxonomy_percentages(
+            df, include=lambda x: not x[0] and x[2], debug=True), indent=4))
+
+        # __debug_check_last_row(df)
 
 
 def get_excel_as_dataframe(filename: str) -> pd.DataFrame:
@@ -70,12 +90,13 @@ def get_excel_as_dataframe(filename: str) -> pd.DataFrame:
     return pd.DataFrame((r[1:] for r in data), index=[r[0] for r in data], columns=[col.split('\n')[0] if col is not None and '\n' in col else col for col in cols])
 
 
-def get_taxonomy(df: pd.DataFrame) -> dict:
+def get_taxonomy(df: pd.DataFrame, include=lambda x: True) -> dict:
     """
     Returns a taxonomy of the annotations in the Excel file.
 
     Args:
         df (pd.DataFrame): The Excel file as a pandas DataFrame.
+        include (lambda, optional): A filter to include only certain annotations. Defaults to including all annotations.
 
     Returns:
         dict: The taxonomy.
@@ -93,7 +114,7 @@ def get_taxonomy(df: pd.DataFrame) -> dict:
             for subcategory in categories[column]:
                 # Remove empty cells
                 taxonomy[column][subcategory] = [
-                    (i + sheet_offset, value, get_scores_of_row(df, i + sheet_offset)) for i, value in enumerate(df[column].tolist()) if value is not None and subcategory in value]
+                    (i + sheet_offset, value, get_scores_of_row(df, i + sheet_offset)) for i, value in enumerate(df[column].tolist()) if value is not None and subcategory in value and include(get_scores_of_row(df, i + sheet_offset))]
 
             # Collect all the undefined annotations
             all_subcategories = [
@@ -105,14 +126,22 @@ def get_taxonomy(df: pd.DataFrame) -> dict:
                             if annotation.strip() not in all_subcategories:
                                 if column not in taxonomy["_undefined"]:
                                     taxonomy["_undefined"][column] = []
-                                taxonomy["_undefined"][column].append(
-                                    (row + sheet_offset, annotation.strip(), get_scores_of_row(df, row + sheet_offset)))
+
+                                correlated_scores = get_scores_of_row(
+                                    df, row + sheet_offset)
+                                if include(correlated_scores):
+                                    taxonomy["_undefined"][column].append(
+                                        (row + sheet_offset, annotation.strip(), correlated_scores))
                     else:
                         if value.strip() not in all_subcategories:
                             if column not in taxonomy["_undefined"]:
                                 taxonomy["_undefined"][column] = []
-                            taxonomy["_undefined"][column].append(
-                                (row + sheet_offset, value.strip(), get_scores_of_row(df, row + sheet_offset)))
+
+                            correlated_scores = get_scores_of_row(
+                                df, row + sheet_offset)
+                            if include(correlated_scores):
+                                taxonomy["_undefined"][column].append(
+                                    (row + sheet_offset, value.strip(), correlated_scores))
 
     return taxonomy
 
@@ -185,9 +214,66 @@ def _get_es_below_x_taxonomy_counts(taxonomy: dict, x: float) -> dict:
     return get_taxonomy_counts(taxonomy, include=lambda x: x[1] < x)
 
 
-def get_cumulative_scores(taxonomy_counts: dict) -> dict:
+def _get_unique_rows_taxonomy_counts(taxonomy: dict, debug: bool = False) -> int:
+    row_numbers = set()
+    for category in taxonomy:
+        if category == "_undefined":
+            for row in taxonomy[category]:
+                row_numbers.add(row[0])  # row[0] is the row number
+            continue
+        for subcategory in taxonomy[category]:
+            for row in taxonomy[category][subcategory]:
+                row_numbers.add(row[0])
+    if debug:
+        disregarded_rows = set()
+        for i in range(1, row_limit + 1):
+            if i not in row_numbers:
+                disregarded_rows.add(i)
+
+        print("\nDisregarded rows:", len(disregarded_rows),
+              "\nIn specific:", disregarded_rows)
+        print("\nIncluded rows:", len(row_numbers),
+              "\nIn specific:", row_numbers, "\n")
+    return len(row_numbers)
+
+
+def get_taxonomy_percentages(taxonomy_counts: dict, divisor: int = row_limit) -> dict:
     """
-    Returns the cumulative scores of the taxonomy.
+    Returns the percentages of the taxonomy.
+
+    Args:
+        taxonomy_counts (dict): The taxonomy counts.
+        divisor (int, optional): The divisor to use for the percentages. Defaults to row_limit.
+
+    Returns:
+        dict: The percentages of the taxonomy.
+    """
+    percentages = {}
+    for category in taxonomy_counts:
+        if category == "_undefined":
+            percentages[category] = taxonomy_counts[category] / \
+                float(divisor) * 100
+            continue
+        percentages[category] = {}
+        for subcategory in taxonomy_counts[category]:
+            percentages[category][subcategory] = taxonomy_counts[category][subcategory] / \
+                float(divisor) * 100
+
+    return percentages
+
+
+def get_filtered_taxonomy_percentages(df: pd.DataFrame, include=lambda x: True, debug: bool = False) -> dict:
+    filtered_taxonomy = get_taxonomy(df, include)
+    return get_taxonomy_percentages(
+        taxonomy_counts=get_taxonomy_counts(filtered_taxonomy),
+        divisor=_get_unique_rows_taxonomy_counts(
+            filtered_taxonomy, debug=debug)
+    )
+
+
+def get_cumulative_counts(taxonomy_counts: dict) -> dict:
+    """
+    Returns the cumulative counts of the taxonomy.
 
     Args:
         taxonomy_counts (dict): The taxonomy counts.
@@ -205,6 +291,35 @@ def get_cumulative_scores(taxonomy_counts: dict) -> dict:
             cumulative_scores[category] += taxonomy_counts[category][subcategory]
 
     return cumulative_scores
+
+
+def get_cumulative_percentages(taxonomy_cumulative_counts: dict, divisor: int = row_limit) -> dict:
+    """
+    Returns the cumulative percentages of the taxonomy.
+
+    Args:
+        taxonomy_cumulative_counts (dict): The taxonomy cumulative counts.
+        divisor (int, optional): The divisor to use for the percentages. Defaults to row_limit.
+
+    Returns:
+        dict: The cumulative percentages of the taxonomy.
+    """
+    cumulative_percentages = {}
+    for category in taxonomy_cumulative_counts:
+        cumulative_percentages[category] = taxonomy_cumulative_counts[category] / \
+            float(divisor) * 100
+
+    return cumulative_percentages
+
+
+def get_filtered_cumulative_percentages(df: pd.DataFrame, include=lambda x: True, debug: bool = False) -> dict:
+    filtered_taxonomy = get_taxonomy(df, include)
+    return get_cumulative_percentages(
+        taxonomy_cumulative_counts=get_cumulative_counts(
+            get_taxonomy_counts(filtered_taxonomy)),
+        divisor=_get_unique_rows_taxonomy_counts(
+            filtered_taxonomy, debug=debug)
+    )
 
 
 def get_scores_of_row(df: pd.DataFrame, row: int) -> (bool, float, bool):
@@ -226,6 +341,20 @@ def get_scores_of_row(df: pd.DataFrame, row: int) -> (bool, float, bool):
     es = df.iloc[row]["ES"]
     other_comments = df.iloc[row]["other comments"]
     return (False if em is None or em == 'False' else True, float(es), False if other_comments is None else "valid" in other_comments)
+
+
+def __debug_check_last_row(df: pd.DataFrame) -> None:
+    """
+    Checks if the last row is correct.
+
+    Args:
+        df (pd.DataFrame): the dataframe
+    """
+    print("Row limit:", row_limit)
+    print("Dataframe length:", len(df))
+
+    last_row = df.iloc[-1:]
+    print("Last row:", last_row)
 
 
 if __name__ == "__main__":
