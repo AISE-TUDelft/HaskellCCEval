a008e387f6a9d69a62a1243d7de91d508fc3c2d9
Tim van Dam <timvandamcs@gmail.com>
2023-10-30 20:10:28 +0100

    XDDDDD

--- models/inference/unixcoder_finetuned_predict.py
+++ models/inference/unixcoder_finetuned_predict.py
@@ -1,7 +1,7 @@
 import os
 from UniXcoder import create_predict_fn
 
-# download these weights from huggingface timvandam/UniXcoder-Haskell
+# download these weights from huggingface timvandam/ML4SE23_G7_UniXcoder-Haskell
 bin_path = os.path.join(os.path.dirname(__file__), "unixcoder_finetuned.bin")
 
 
